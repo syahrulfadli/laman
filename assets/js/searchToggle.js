41ddbd6f9bd5d67ebce086e6a6942fb32479825c
syahrulfadli <callsyahrul@outlook.com>
2020-08-05 21:41:30 +0800

    update for more accesbility

--- assets/js/searchToggle.js
+++ assets/js/searchToggle.js
@@ -2,17 +2,17 @@ var x = document.getElementById("search-hide");
 var z = document.getElementById("search-toggle");
 var a = document.getElementById("close");
 function searchToggle() {
-  if (x.style.left === "-100px" && x.style.width === "0px") {
+  if (x.style.left === "-100px" && x.style.width === "100px") {
     x.style.left = "0px";
     x.style.width = "100%";
     x.style.visibility = "visible";
     z.style.transform = "scale(0.1)";
     z.style.visibility = "hidden";
     a.style.visibility = "visible";
-    a.style.transform = "scale(1.4)";
+    a.style.transform = "scale(1.1)";
   } else {
     x.style.left = "-100px";
-    x.style.width= "0px";
+    x.style.width= "100px";
     x.style.visibility = "hidden";
     z.style.transform = "scale(1)";
     z.style.visibility = "visible";
@@ -23,7 +23,7 @@ function searchToggle() {
 function closeToggle(){
 if (x.style.left === "0px" && x.style.width === "100%"){
     x.style.left = "-100px";
-    x.style.width= "0px";
+    x.style.width= "100px";
     x.style.visibility = "hidden";
     z.style.transform = "scale(1)";
     z.style.visibility = "visible";
@@ -36,6 +36,6 @@ if (x.style.left === "0px" && x.style.width === "100%"){
     z.style.transform = "scale(0.1)";
     z.style.visibility = "hidden";
     a.style.visibility = "visible";
-    a.style.transform = "scale(1.4)";
+    a.style.transform = "scale(1.1)";
 }
 }
