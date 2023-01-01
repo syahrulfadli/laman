a08713e6c7115b8e6a4e75bef63067765e7030cc
syahrulfadli <callsyahrul@outlook.com>
2023-01-01 13:57:52 +0700

    update theme

--- assets/js/searchToggle.js
+++ assets/js/searchToggle.js
@@ -1,42 +1,8 @@
-var x = document.getElementById("search-hide");
-var z = document.getElementById("search-toggle");
-var a = document.getElementById("close");
+const searchForm = document.getElementById("search-form");
+const searchIcon = document.getElementById("searchIcon");
+const closeIcon = document.getElementById("closeIcon");
 function searchToggle() {
-  if (x.style.opacity === "0") {
-    x.style.opacity = "1";
-    x.style.visibility = "visible";
-    x.style.transform = "scale(1)";
-    z.style.transform = "scale(0.1)";
-    z.style.visibility = "hidden";
-    a.style.visibility = "visible";
-    a.style.transform = "scale(1.4)";
-
-  } else {
-    x.style.opacity = "0";
-    x.style.visibility = "hidden";
-    x.style.transform = "scale(0.1)";
-    z.style.transform = "scale(1)";
-    z.style.visibility = "visible";
-    a.style.visibility = "hidden";
-    a.style.transform = "scale(0.1)";
-  }
-}
-function closeToggle(){
-if (x.style.opacity === "1"){
-    x.style.opacity = "0";
-    x.style.visibility = "hidden";
-    x.style.transform = "scale(0.1)";
-    z.style.transform = "scale(1)";
-    z.style.visibility = "visible";
-    a.style.visibility = "hidden";
-    a.style.transform = "scale(0.1)";
-}else{
-    x.style.opacity = "1";
-    x.style.visibility = "visible";
-    z.style.transform = "scale(0.1)";
-    x.style.transform = "scale(1)";
-    z.style.visibility = "hidden";
-    a.style.visibility = "visible";
-    a.style.transform = "scale(1.4)";
-}
+  closeIcon.classList.toggle("hidden")
+  searchIcon.classList.toggle("hidden")
+  searchForm.classList.toggle("hidden")
 }
