c331b7670d8845f167d632c3bef41738401825d3
syahrulfadli <callsyahrul@outlook.com>
2023-01-01 19:22:05 +0700

    update theme

--- assets/js/searchToggle.js
+++
@@ -1,8 +0,0 @@
-const searchForm = document.getElementById("search-form");
-const searchIcon = document.getElementById("searchIcon");
-const closeIcon = document.getElementById("closeIcon");
-function searchToggle() {
-  closeIcon.classList.toggle("hidden")
-  searchIcon.classList.toggle("hidden")
-  searchForm.classList.toggle("hidden")
-}
