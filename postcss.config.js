f9dcc6a0bf767f266170331729798929845caaeb
syahrulfadli <callsyahrul@outlook.com>
2023-01-08 08:43:28 +0700

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./hugo_stats.json"],
+    defaultExtractor: (content) => {
+      const els = JSON.parse(content).htmlElements;
+      return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+    },
+    safelist: [],
+  });
+
 module.exports = {
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
     ]
   }
